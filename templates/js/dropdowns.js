4994dabe162bcb670a1b827cd5768d6721cd0ca4
Jenni Oishee <jennioishee@Jennis-MacBook-Pro.local>
2025-09-19 00:10:12 -0500

    Submitting new changes

--- templates/js/dropdowns.js
+++ templates/js/dropdowns.js
@@ -3,10 +3,9 @@ import { loadDescription } from "./description.js";
 
 export function populateBonesetDropdown(bonesets) {
     const bonesetSelect = document.getElementById("boneset-select");
-    bonesetSelect.innerHTML = '<option value="">--Please select a Boneset--</option>';
-
-    bonesets.forEach(set => {
-        const option = document.createElement('option');
+    bonesetSelect.innerHTML = "<option value=\"\">--Please select a Boneset--</option>"; 
+        bonesets.forEach(set => {
+        const option = document.createElement("option");
         option.value = set.id;
         option.textContent = set.name;
         bonesetSelect.appendChild(option);
@@ -14,20 +13,20 @@ export function populateBonesetDropdown(bonesets) {
 }
 
 export function setupDropdownListeners(combinedData) {
-    const bonesetSelect = document.getElementById('boneset-select');
-    const boneSelect = document.getElementById('bone-select');
-    const subboneSelect = document.getElementById('subbone-select');
+    const bonesetSelect = document.getElementById("boneset-select");
+    const boneSelect = document.getElementById("bone-select");
+    const subboneSelect = document.getElementById("subbone-select");
 
-    bonesetSelect.addEventListener('change', (e) => {
+    bonesetSelect.addEventListener("change", (e) => {
         const selectedBonesetId = e.target.value;
 
-        boneSelect.innerHTML = '<option value="">--Please select a Bone--</option>';
-        subboneSelect.innerHTML = '<option value="">--Please choose a Sub-Bone--</option>';
+        boneSelect.innerHTML = "<option value=\"\">--Please select a Bone--</option>";
+        subboneSelect.innerHTML = "<option value=\"\">--Please choose a Sub-Bone--</option>";
         subboneSelect.disabled = true;
 
         const relatedBones = combinedData.bones.filter(b => b.boneset === selectedBonesetId);
         relatedBones.forEach(bone => {
-            const option = document.createElement('option');
+            const option = document.createElement("option");
             option.value = bone.id;
             option.textContent = bone.name;
             boneSelect.appendChild(option);
@@ -37,13 +36,13 @@ export function setupDropdownListeners(combinedData) {
        // if (selectedBonesetId) loadDescription(selectedBonesetId);
     });
 
-    boneSelect.addEventListener('change', (e) => {
+    boneSelect.addEventListener("change", (e) => {
         const selectedBoneId = e.target.value;
 
-        subboneSelect.innerHTML = '<option value="">--Please choose a Sub-Bone--</option>';
+        subboneSelect.innerHTML = "<option value=\"\">--Please choose a Sub-Bone--</option>";
         const relatedSubbones = combinedData.subbones.filter(sb => sb.bone === selectedBoneId);
         relatedSubbones.forEach(sb => {
-            const option = document.createElement('option');
+            const option = document.createElement("option");
             option.value = sb.id;
             option.textContent = sb.name;
             subboneSelect.appendChild(option);
@@ -53,7 +52,7 @@ export function setupDropdownListeners(combinedData) {
         if (selectedBoneId) loadDescription(selectedBoneId);
     });
 
-    subboneSelect.addEventListener('change', (e) => {
+    subboneSelect.addEventListener("change", (e) => {
         const selectedSubboneId = e.target.value;
         if (selectedSubboneId) loadDescription(selectedSubboneId);
     });
